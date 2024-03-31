70403892c623eddf8bbd39d4b839d9e3cab4cc7b
dihanto <dihanto2306@gmail.com>
2024-03-31 18:37:43 +0700

    ✨ configure submit message data

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -4,14 +4,12 @@ export default function Message() {
 
     const formElement = document.querySelector("form");
     const formData = new FormData(formElement);
-    console.log(formData);
     try {
       const response = await fetch(
-        "https://script.google.com/macros/s/AKfycbwOB1BpeuqHkmb5keWleXthadJA8YUT0O9NQHo6hwCBURfKIMYwjUYzL-n0FIuVTKV8/exec",
+        "https://script.google.com/macros/s/AKfycbz9rlSr_b6nZHVzjGgateAMYqWx9HcbD-CYqeS11MjYnb1ON6gnOc6ol6B8Qmg4pWdP/exec",
         {
           method: "POST",
           body: formData,
-          mode: "no-cors",
         }
       );
       if (response.ok) {
@@ -41,21 +39,30 @@ export default function Message() {
       </p>
 
       <form className=" p-6 w-full form" onSubmit={(e) => handleSubmit(e)}>
-        <h2 className="text-lg font-gabarito text-slate-600 text-left">
-          Kirim pesan untuk Romeo & Juliet
-        </h2>
-        <input
-          type="text"
-          placeholder="name"
-          name="Name"
-          className="w-full p-1 mb-1"
-        ></input>
-        <input
-          type="text"
-          className="w-full p-1"
-          name="Message"
-          placeholder="message"
-        ></input>
+        <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+          <div className="sm:col-span-4">
+            <div className="mt-2">
+              <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  autoComplete="name"
+                  className="block flex-1 border-0 bg-transparent py-1.5 px-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                  placeholder="Nama"
+                />
+              </div>
+              <div className="mt-2">
+                <textarea
+                  id="message"
+                  name="message"
+                  rows="3"
+                  className="block w-full rounded-md border-0 py-1.5 px-1 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                ></textarea>
+              </div>
+            </div>
+          </div>
+        </div>
         <button type="submit" className="border border-black mt-3 px-3 py-1">
           Kirim
         </button>
